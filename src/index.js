365b2c2b9500992b5763a59e7d51092e5a1b2f08
CodewShassh <134398035+Snapdragon8g2@users.noreply.github.com>
2025-03-10 21:30:35 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SolveTheCube from './SolveTheCube.tsx';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SolveTheCube />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
